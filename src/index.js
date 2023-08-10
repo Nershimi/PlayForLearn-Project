eb28a4a7b47fb2b1ec4d29683aef7028c5549026
ner <nershimi@gmail.com>
2023-08-10 15:27:15 +0300

    feature/sign-in: - Deleted ApiKey

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import {
   getAuth,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-// import { getFirestore } from "firebase/firestore";
-
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyAr6V2pSlphIWgPGiYVmV_4EAkxxHCacMg",
-  authDomain: "learntoplay-f2f2c.firebaseapp.com",
-  projectId: "learntoplay-f2f2c",
-  storageBucket: "learntoplay-f2f2c.appspot.com",
-  messagingSenderId: "717817592020",
-  appId: "1:717817592020:web:646e69e6d01d8c91fca341",
-  measurementId: "G-4QRWQJ1KXX",
-});
 
 const auth = getAuth(firebaseApp);
 onAuthStateChanged(auth, (user) => {
