74d1e5b706e9b46ff6ac733ffbdb9ec5a03091f0
ner <nershimi@gmail.com>
2023-09-03 20:24:06 +0300

    feature/sign-in:  - Added auth module

--- src/initializeApp.js
+++ src/initializeApp.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 const auth = getAuth(firebaseApp);
 onAuthStateChanged(auth, (user) => {
