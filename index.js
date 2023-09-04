4c74465538cf9d639fcfecd3a0140e442e7019ba
ner <nershimi@gmail.com>
2023-09-04 15:25:39 +0300

    feature/sign-in:  - added import to index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+
+
+const auth = getAuth(firebaseApp);
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log("logged in!");
+  } else {
+    console.log("No user!");
+  }
+});
